622ab47f0d2062fcc02cec1f4b8d05539255ba6b
masterhamzaa <master.hamza4576@gmail.com>
2022-11-07 11:23:47 +0100

    add https

--- src/data/wapi.js
+++ src/data/wapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const urll = "http://api.openweathermap.org/data/2.5/weather?";
+const urll = "https://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "291fa4d5cda19204d3ac1050797439e8";
 
 export const statistic = async (city) => {
